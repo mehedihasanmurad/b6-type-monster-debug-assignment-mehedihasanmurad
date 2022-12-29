420d1206b801aa7e940813e89d15539602793f78
Mehedi Hasan Murad <mehedihasna500422@gamil.com>
2022-12-29 09:42:17 +0600

    fourth problem solved

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = parseInt((finishTime - startTime) / 1000);
+  const timeTaken = (finishTime - startTime) / 1000;
 
   // show result modal
   resultModal.innerHTML = "";
@@ -141,7 +141,7 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent = parseInt((currentTime - startTime) / 1000)
+  const timeSpent = (currentTime - startTime) / 1000;
   
 
   document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0
